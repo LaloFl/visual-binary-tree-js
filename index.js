002ee975aa326868f4ed1cc7eb161c93ae3077af
diego <defsdiego@gmail.com>
2021-11-23 01:06:42 -0800

    added changes to button

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var nodeCount = 0;
+nodes = []
 
-function addNode(type) {
+function addNode(number, type) {
+    nodes.push(number)
     var newNode = document.createElement("span");
     newNode.setAttribute("class", `node ${type == undefined ? "" : type}`);
-    newNode.setAttribute("id", "node" + nodeCount);
-    newNode.innerHTML = nodeCount;
+    newNode.setAttribute("id", "node" + nodes.length);
+    newNode.innerHTML = number;
     document.getElementById("root").appendChild(newNode);
-    nodeCount++;
     return newNode;
 }
 
-addNode();
+addNode(1, "root");
+
+document.getElementById("add").addEventListener("click", function () {
+    let newType = "left"
+    let value = document.getElementById("number").value;
+    addNode(value, newType);
+});
